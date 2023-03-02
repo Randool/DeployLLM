a0079fe89f155aaa1816ecef9924ff9fb15e7996
randool <dlf43@qq.com>
2023-03-02 15:12:34 +0800

    Smarter device_map; Refactor model inference queue

--- deploy_llm.py
+++ deploy_llm.py
@@ -11,16 +11,17 @@ import threading
 import time
 from dataclasses import dataclass, field
 from os.path import expanduser, isdir, join
-from typing import List
+from typing import List, Union
 
 import psutil
 import torch
+from accelerate import infer_auto_device_map, init_empty_weights
 from flask import Flask, jsonify, request
-from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, HfArgumentParser, PreTrainedModel, \
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, GenerationConfig, HfArgumentParser, \
+    PreTrainedModel, \
     PreTrainedTokenizer
 
-from utils import show_args, get_logger
-
+from utils import get_logger, show_args
 
 logger = get_logger(__name__, exp_dir=None)
 
@@ -28,11 +29,13 @@ logger = get_logger(__name__, exp_dir=None)
 @dataclass
 class ModelArguments:
     model_name: str = field(default="bigscience/bloomz-3b", metadata={"help": "Model name in HuggingFace model hub"})
-    torch_dtype: str = field(default="auto", metadata={"choices": ["auto", "fp16", "fp32"]})
+    torch_dtype: str = field(default="fp32", metadata={"choices": ["auto", "fp16", "fp32"]})
     device_map: str = field(default="auto", metadata={
         "choices": ["auto", "balanced", "balanced_low_0", "sequential"],
         "help": "https://huggingface.co/docs/accelerate/usage_guides/big_modeling#designing-a-device-map"
     })
+    gpu_max_memory: str = field(default="6GiB")
+    cpu_max_memory: str = field(default="8GiB")
     offload_folder: str = field(default="/tmp/offload")
 
     @property
@@ -41,6 +44,25 @@ class ModelArguments:
             return self.model_name
         HOME = expanduser("~")
         return join(HOME, "Dataset/transformers", self.model_name)
+
+    def get_torch_dtype(self):
+        if self.torch_dtype == "auto":
+            return self.torch_dtype
+        elif self.torch_dtype == "fp16":
+            return torch.float16
+        return torch.float32
+
+    def get_device_map(self) -> Union[str, dict]:
+        """ Smarter device_map """
+        if self.device_map != "auto":
+            return self.device_map
+
+        with init_empty_weights():
+            model_config = AutoConfig.from_pretrained(self.checkpoint)
+            empty_model = AutoModelForCausalLM.from_config(model_config)
+            device_map = infer_auto_device_map(empty_model,
+                                               max_memory={0: self.gpu_max_memory, "cpu": self.cpu_max_memory})
+        return device_map
 
 
 @dataclass
@@ -60,6 +82,19 @@ class DeployArguments:
     timeout: int = field(default=0.200, metadata={"help": "Timeout second"})
 
 
+def parse_args():
+    # Parse Arguments
+    parser = HfArgumentParser([ModelArguments, GenerationArguments, DeployArguments])
+    model_args, gen_args, deploy_args = parser.parse_args_into_dataclasses()
+    model_args: ModelArguments
+    gen_args: GenerationArguments
+    deploy_args: DeployArguments
+    show_args(model_args, logger)
+    show_args(gen_args, logger)
+    show_args(deploy_args, logger)
+    return model_args, gen_args, deploy_args
+
+
 def load_tokenizer_and_model(model_args: ModelArguments):
     gpu_before = torch.cuda.memory_allocated()
     cpu_before = psutil.virtual_memory().used
@@ -67,8 +102,8 @@ def load_tokenizer_and_model(model_args: ModelArguments):
     tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(model_args.checkpoint)
     model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(
         model_args.checkpoint,
-        device_map=model_args.device_map,
-        torch_dtype=model_args.torch_dtype,
+        device_map=model_args.get_device_map(),
+        torch_dtype=model_args.get_torch_dtype(),
         offload_folder=model_args.offload_folder,
     ).eval()
 
@@ -93,68 +128,84 @@ class GeneratedItem:
     generated_text: str
 
 
-def do_generate(tokenizer: PreTrainedTokenizer, model: PreTrainedModel, batch: List[PromptItem],
-                generation_config: GenerationConfig) -> List[GeneratedItem]:
-    batch_text = [item.prompt for item in batch]
-    input_ids = tokenizer(batch_text, return_tensors="pt", padding=True)["input_ids"].to(0)
-    output = model.generate(input_ids, generation_config=generation_config)
-    batch_generated_text = tokenizer.batch_decode(output)
-    results = [
-        GeneratedItem(item.timestamp, time.time(), item.prompt, gen_text[len(item.prompt):]) for item, gen_text in
-        zip(batch, batch_generated_text)
-    ]
-    return results
+class ModelInferenceQueue:
+    def __init__(self, tokenizer: PreTrainedTokenizer, model: PreTrainedModel,
+                 deploy_args: DeployArguments, gen_args: GenerationArguments):
+        self.tokenizer = tokenizer
+        self.model = model
 
+        self.deploy_args = deploy_args
+        self.gen_args = gen_args
+        self.gen_config = GenerationConfig(**vars(self.gen_args))
 
-def inference(tokenizer: PreTrainedTokenizer, model: PreTrainedModel, prompt_queue: queue.Queue, result_pool: dict,
-              deploy_args: DeployArguments, generation_config: GenerationConfig):
-    """
-    Realize the following requirements.
-    1. executing inference immediately when the inference demand is greater than or equal to the batch size.
-    2. execute inference immediately when the request time in the queue exceeds timeout.
-    """
-    batch = []
-    next_batch = []
+        # Message Queue & Result Pool
+        self._queue = queue.Queue()
+        self._result_pool = {}
 
-    while True:
-        try:
-            item: PromptItem = prompt_queue.get(block=True, timeout=deploy_args.timeout)
-        except queue.Empty:
-            continue
+        self._last_infer_time = 0
+        self._lock = threading.Lock()
+        self._condition = threading.Condition(self._lock)
 
-        batch.append(item)
-        time.sleep(deploy_args.timeout)
+        self.worker_thread = threading.Thread(target=self._run, daemon=True)
 
-        while not prompt_queue.empty() and len(batch) < deploy_args.batch_size:
-            curr_item = prompt_queue.get()
-            if curr_item.timestamp - item.timestamp >= deploy_args.timeout:
-                next_batch.append(curr_item)
-                break
-            else:
-                batch.append(curr_item)
+    def enqueue(self, item: PromptItem):
+        """ When the new request is coming, `notify` will notify the working thread """
+        with self._lock:
+            self._queue.put(item)
+            self._last_infer_time = time.monotonic()
+            self._condition.notify()
 
-        generate_items = do_generate(tokenizer, model, batch, generation_config)
-        for item in generate_items:
-            result_pool[item.timestamp] = item
-        batch.clear()
-        batch.extend(next_batch)
-        next_batch.clear()
+    def dequeue(self, timestamp: float) -> GeneratedItem:
+        # TODO: Block-able Dict
+        while timestamp not in self._result_pool:
+            time.sleep(0.05)
+        return self._result_pool.pop(timestamp)
+
+    def _do_inference(self, batch: List[PromptItem]):
+        batch_text = [item.prompt for item in batch]
+        input_ids = self.tokenizer(batch_text, return_tensors="pt", padding=True)["input_ids"].to(0)
+        output = self.model.generate(input_ids, generation_config=self.gen_config)
+        batch_generated_text = self.tokenizer.batch_decode(output)
+
+        for item, gen_text in zip(batch, batch_generated_text):
+            gen_item = GeneratedItem(item.timestamp, time.time(), item.prompt, gen_text[len(item.prompt):])
+            self._result_pool[item.timestamp] = gen_item
+
+    def start(self):
+        self.worker_thread.start()
+
+    def _run(self):
+        while True:
+            with self._lock:
+                # Waiting
+                while self._queue.qsize() < self.deploy_args.batch_size and \
+                        time.monotonic() - self._last_infer_time < self.deploy_args.timeout:
+                    self._condition.wait(timeout=self.deploy_args.timeout / 10)
+
+                # Batch size branch
+                if self._queue.qsize() >= self.deploy_args.batch_size:
+                    batch_list = []
+                    while not self._queue.empty() and len(batch_list) < self.deploy_args.batch_size:
+                        batch_list.append(self._queue.get())
+                    self._last_infer_time = time.monotonic()
+                    self._do_inference(batch_list)
+
+                # Timeout branch
+                elif time.monotonic() - self._last_infer_time >= self.deploy_args.timeout:
+                    if not self._queue.empty():
+                        batch_list = []
+                        while not self._queue.empty():
+                            batch_list.append(self._queue.get())
+                        self._last_infer_time = time.monotonic()
+                        self._do_inference(batch_list)
 
 
 def main():
-    # Parse Arguments
-    parser = HfArgumentParser([ModelArguments, GenerationArguments, DeployArguments])
-    model_args, gen_args, deploy_args = parser.parse_args_into_dataclasses()
-    model_args: ModelArguments
-    gen_args: GenerationArguments
-    deploy_args: DeployArguments
-    show_args(model_args, logger)
-    show_args(gen_args, logger)
-    show_args(deploy_args, logger)
+    model_args, gen_args, deploy_args = parse_args()
 
-    # Prepare Message Queue & Result Pool
-    prompt_queue = queue.Queue()
-    result_pool = {}
+    # Prepare Language Model
+    tokenizer, model = load_tokenizer_and_model(model_args)
+    inference_queue = ModelInferenceQueue(tokenizer, model, deploy_args, gen_args)
 
     # Set up Flask
     app = Flask(__name__)
@@ -168,56 +219,38 @@ def main():
         }
         return jsonify(state)
 
+    @app.route("/model", methods=["GET"])
+    def _model():
+        return str(model)
+
+    @app.route("/tokenizer", methods=["GET"])
+    def _tokenizer():
+        return str(tokenizer)
+
     @app.route('/inference', methods=["POST"])
     def _inference():
         timestamp = time.time()
         raw_data = request.get_data().decode()
         logger.info(f"{request}\t{raw_data}")
-        
+
         try:
             prompt = json.loads(raw_data)["prompt"]
         except Exception as e:
             return str(e)
 
-        prompt_queue.put(PromptItem(timestamp, prompt))
-
-        # TODO: Block-able Dict
-        while timestamp not in result_pool:
-            time.sleep(0.05)
-
-        response = vars(result_pool.pop(timestamp))
+        inference_queue.enqueue(PromptItem(timestamp, prompt))
+        response = vars(inference_queue.dequeue(timestamp))
         logger.info(response)
         return jsonify(response)
 
-
-    # Prepare Language Model
-    tokenizer, model = load_tokenizer_and_model(model_args)
-    generation_config = GenerationConfig(**vars(gen_args))
-
-    # Background threading does inference
-    generate_thread = threading.Thread(
-        target=inference,
-        kwargs={
-            "tokenizer": tokenizer,
-            "model": model,
-            "prompt_queue": prompt_queue,
-            "result_pool": result_pool,
-            "deploy_args": deploy_args,
-            "generation_config": generation_config,
-        },
-        daemon=True,
-    )
-
-    # Start threadings
-    generate_thread.start()
+    # Start threading
+    inference_queue.start()
 
     try:
         app.run(host=deploy_args.host, port=deploy_args.port, debug=False)
     except KeyboardInterrupt:
         logger.info("KeyboardInterrupt")
         exit(0)
-
-    generate_thread.join()
 
 
 if __name__ == "__main__":
